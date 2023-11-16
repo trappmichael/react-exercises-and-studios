be3d0d165fe6ce13140ab2120f8bf81f0ab09b53
trappmichael <142745435+trappmichael@users.noreply.github.com>
2023-11-15 20:25:40 -0600

    Completed Exercise 1 Part 1

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Books I Read This Month";
+   let book1 = "https://pictures.abebooks.com/isbn/9780441005901-us.jpg";
+   let book2 = "https://pictures.abebooks.com/inventory/md/md31653683860.jpg";
+   let book3 = "https://pictures.abebooks.com/isbn/9780679736325-us.jpg";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="Dune" />
+         <img src={book2} alt="My Brilliant Friend" />
+         <img src={book3} alt="Suttree" />
       </div>      
    );
 }
