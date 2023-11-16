2c4e8ff3d697a847e47ae4775faf32c423703275
trappmichael <142745435+trappmichael@users.noreply.github.com>
2023-11-16 14:22:17 -0600

    React Part 1 Studio Complete

--- part1/studio/src/App.jsx
+++ part1/studio/src/App.jsx
@@ -2,15 +2,19 @@ import { useState } from 'react'
 import './App.css'
 import RecipeDescription from './components/Description.jsx';
 import RecipeIngredients from './components/Ingredients.jsx';
+import RecipePhoto from './components/Photos.jsx';
 
 function App() {
 
   return (
     <>
       <div className="App">
-        <div>
-          <RecipeDescription/>
-          <RecipeIngredients/>
+        <div className="recipePhotoBlock">
+          <RecipePhoto/>
+          <div className="recipePhotoBlock">
+            <RecipeDescription/>
+            <RecipeIngredients/>
+          </div>
         </div>
       </div>
     </>
---
+++ part1/studio/src/components/Photos.jsx
@@ -0,0 +1,7 @@
+import styles from './Description.module.css';
+
+export default function RecipePhoto() {
+    return (
+        <img src="https://alexandracooks.com/wp-content/uploads/2017/02/allthecomponents.jpg" alt="Chicken Shawarma picture" className = {styles.imageUpdates} />
+    );
+};
